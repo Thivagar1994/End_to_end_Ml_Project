c0beb90326e9de5c6b01a8baa02d9e27ba4a3863
Thivagar1994 <thivagarneelamegam@gmail.com>
2025-08-31 09:49:16 +0530

    utils added

---
+++ src/mlProject/utils/common.py
@@ -0,0 +1,109 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from mlProject import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (Path): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded succesfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
+@ensure_annotations 
+def save_json(path: Path, data: dict):
+    """save json data
+
+    Args:
+        path (Path): path to save json file
+        data (dict): data to saved in json file
+    """
+    with open(path, "w") as f:
+            json.dump(data, f, indent=4)
+            
+    logger.info(f"json file saved at: {path}")
+    
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+
+    Args:
+        path (Path): path to json file
+
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file loaded successfully from: {path}")
+    return ConfigBox(content)
+
+import joblib
+from pathlib import Path
+from ensure import ensure_annotations
+from typing import Any
+import logging
+
+logger = logging.getLogger(__name__)
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """save binary file
+
+    Args:
+        data (Any): data to be saved as binary
+        path (Path): path to binary file
+    """
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at: {path}")
+
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """load binary data
+
+    Args:
+        path (Path): path to binary file
+
+    Returns:
+        Any: object stored in binary file
+    """
+    data = joblib.load(path)
+    logger.info(f"binary file loaded successfully from: {path}")
+    return data
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path) / 1024)
+    return f"~ {size_in_kb} KB"
+    
